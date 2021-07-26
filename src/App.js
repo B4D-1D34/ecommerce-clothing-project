e2c4cfc60d9f37507d6c3135d096e7e4497e249a
B4D-1D34 <rapkor4@gmail.com>
2021-07-27 04:23:00 +0500

    routing, a lot of new components, shop page, sign in form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
-import HomePage from "./homepage.component";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import "./App.css";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUpPage} />
+      </Switch>
     </div>
   );
 }

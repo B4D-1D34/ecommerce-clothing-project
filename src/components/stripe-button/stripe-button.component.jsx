0cf9652f33224305d6b93657e4218fe4286903fa
B4D-1D34 <rapkor4@gmail.com>
2021-08-02 03:58:48 +0500

    more data flow optimizations with redux, stripe payments in test mode implemented

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51JJmQVCNLhDpjJpAmHXJc0RvrAmJG33DOo6qNFbtZQcbzhEyxeLKBM8KXSeMMXk9Yeuddbdz3Jn4kez0j86fXBz300m12wyQyJ";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Crown Clothing."
+      billingAddress
+      shippingAddress
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;

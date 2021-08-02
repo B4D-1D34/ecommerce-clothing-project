12278fed0eb59ab8807d5faeb02f1e364ce7eff1
B4D-1D34 <rapkor4@gmail.com>
2021-08-02 16:18:38 +0500

    removed redux-logger from production build

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -6,7 +6,6 @@ import CollectionPage from "../collection/collection.component";
 import "./shop.styles.scss";
 
 const ShopPage = ({ match }) => {
-  console.log(match);
   return (
     <div className="shop-page">
       <Route exact path={`${match.path}`} component={CollectionsOverview} />
